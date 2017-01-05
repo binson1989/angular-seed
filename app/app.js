9693487926347c239d3cbae3ec20bf87207609b2
binson1989 <binson1989@gmail.com>
2017-01-06 01:51:19 +0530

    changes

--- app/app.js
+++ app/app.js
@@ -1,14 +1,24 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+// angular.module('myApp', [
+//   'ngRoute',
+//   'myApp.view1',
+//   'myApp.view2',
+//   'myApp.version'
+// ]).
+// config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+//   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+//   $routeProvider.otherwise({redirectTo: '/view1'});
+// }]);
+
+
+angular.module('myApp', [])
+  .controller('MyController', ['$scope', function ($scope) {
+    $scope.greetMe = 'Binson';
+  }]);
+
+angular.element(function () {
+  angular.bootstrap(document, ['myApp']);
+});
