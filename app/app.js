2a61e1698748a992c3af1bb5ec75a4a32987af78
binson1989 <binson1989@gmail.com>
2017-01-09 00:55:19 +0530

    manully bootstrapping angular app

--- app/app.js
+++ app/app.js
@@ -1,24 +1,69 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
+
+var myApplication = angular.module("myApplication", [])
+    .controller('myCtrl', function ($scope) {
+        console.log('Controller initialized');
+    });
+
+fetchData().then(bootstrapApplication);
+
+function fetchData() {
+    var initInjector = angular.injector(["ng"]);
+    var $http = initInjector.get("$http");
+
+    return $http.get("http://localhost:2900/api/categories").then(function (response) {
+        myApplication.constant("config", response.data);
+    }, function (errorResponse) {
+        // Handle error case
+    });
+}
+
+function bootstrapApplication() {
+    angular.element(document).ready(function () {
+        angular.bootstrap(document, ["myApplication"]);
+    });
+}
+
+//Declare app level module which depends on views, and components
 // angular.module('myApp', [
 //   'ngRoute',
 //   'myApp.view1',
 //   'myApp.view2',
 //   'myApp.version'
 // ]).
-// config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+// config(['$locationProvider', '$routeProvider', '$http', function($locationProvider, $routeProvider, $http) {
 //   $locationProvider.hashPrefix('!');
+
+
 
 //   $routeProvider.otherwise({redirectTo: '/view1'});
 // }]);
 
 
-angular.module('myApp', [])
-  .controller('MyController', ['$scope', function ($scope) {
-    $scope.greetMe = 'Binson';
-  }]);
+// angular.module('myApp', [])
+//   .controller('MyController', ['$scope', function ($scope) {
+//     $scope.greetMe = 'Binson';
+//   }]);
 
-angular.element(function () {
-  angular.bootstrap(document, ['myApp']);
-});
+// angular.element(function () {
+//   angular.bootstrap(document, ['myApp']);
+// });
+
+// var myApp = angular.module('myApp', ['ngRoute']);
+
+// myApp.config(function ($routeProvider) {
+//     console.log("app config");
+// });
+
+// myApp.run(function () {
+//     console.log("app run");
+// });
+
+// myApp.controller('myCtrl', function($scope) {
+//     console.log("app controller");
+// });
+
+
+
+
